a53bdca8b1722f2fd5fcdaebbedc18422fd5749d
Gabriel Minucci <gabriel@minucci.com.br>
2016-08-23 15:10:48 -0300

    Created Parking related model classes

--- src/bike_parking/parking/models.py
+++ src/bike_parking/parking/models.py
@@ -1,5 +1,43 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+# Models Enum declarations
+parking_space_status = (
+    ('idle', 'Livre'),
+    ('leased', 'Alugado'),
+    ('occupied', 'Ocupado'),
+    ('broken', 'Quebrado'),
+)
+
+
+class Location(models.Model):
+    cep = models.CharField('CEP', max_length=15)
+    street = models.CharField('Endereco', max_length=100)
+    number = models.IntegerField('Numero')
+    neighborhood = models.CharField('Bairro', max_length=50)
+    city = models.CharField('Cidade', max_length=100)
+    complement = models.CharField('Complemento', max_length=100)
+    latitude = models.FloatField('Latitude')
+    longitude = models.FloatField('Longitude')
+
+
+class ParkingSpace(models.Model):
+    status = models.CharField(choices=parking_space_status, max_length=20)
+
+
+class ParkingLot(models.Model):
+    location = models.ForeignKey(Location)
+    parking_spaces = models.ForeignKey(ParkingSpace, on_delete=models.CASCADE)
+    description = models.TextField('Descricao')
+    default_price = models.FloatField('Preco padrao')
+    per_hour_price = models.FloatField('Preco por hora')
+    active = models.BooleanField('Ativo')
+
+
+class Bicycle(models.Model):
+    parking_space = models.ForeignKey(ParkingSpace)
+    model = models.CharField('Modelo', max_length=100)
+    serial_number = models.CharField('Numero de serie', max_length=100)
+
+
+
