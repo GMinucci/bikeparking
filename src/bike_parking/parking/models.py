ece4777f21187da1b34e8382eb202b0dba15307d
Gabriel Minucci <gabriel@minucci.com.br>
2016-08-23 16:41:10 -0300

    Updated ParkingLot, ParkingSpace and Location models

--- src/bike_parking/parking/models.py
+++ src/bike_parking/parking/models.py
@@ -16,28 +16,60 @@ class Location(models.Model):
     number = models.IntegerField('Numero')
     neighborhood = models.CharField('Bairro', max_length=50)
     city = models.CharField('Cidade', max_length=100)
+    state = models.CharField('Estado', max_length=100)
+    country = models.CharField('Pais', max_length=100)
     complement = models.CharField('Complemento', max_length=100)
     latitude = models.FloatField('Latitude')
     longitude = models.FloatField('Longitude')
 
+    class Meta:
+        verbose_name = 'Localizacao'
+        verbose_name_plural = 'Localizacoes'
+
+    def __unicode__(self):
+        return "%s, %s" % (self.street, self.number)
+
 
 class ParkingLot(models.Model):
     location = models.ForeignKey(Location)
+    name = models.CharField('Nome', max_length=100)
     description = models.TextField('Descricao')
     default_price = models.FloatField('Preco padrao')
     per_hour_price = models.FloatField('Preco por hora')
     active = models.BooleanField('Ativo')
 
+    class Meta:
+        verbose_name = 'Estacionamento'
+        verbose_name_plural = 'Estacionamentos'
+
+    def __unicode__(self):
+        return self.name
+
 
 class ParkingSpace(models.Model):
     parking_lot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE, related_name='parking_lot')
+    number = models.IntegerField('Numero')
     status = models.CharField(choices=parking_space_status, max_length=20)
+
+    class Meta:
+        verbose_name = 'Vaga'
+        verbose_name_plural = 'Vagas'
+
+    def __unicode__(self):
+        return "%i - %s" % (self.number, self.parking_lot)
 
 
 class Bicycle(models.Model):
     parking_space = models.ForeignKey(ParkingSpace)
     model = models.CharField('Modelo', max_length=100)
     serial_number = models.CharField('Numero de serie', max_length=100)
+
+    class Meta:
+        verbose_name = 'Bicicleta'
+        verbose_name_plural = 'Bicicletas'
+
+    def __unicode__(self):
+        return self.model
 
 
 
