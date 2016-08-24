25735319f7914b90f36f5b5503b1edf06c603b25
Gabriel Minucci <gabriel@minucci.com.br>
2016-08-24 16:37:29 -0300

    Added Index, Login and Test pages

--- src/bike_parking/website/urls.py
+++ src/bike_parking/website/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.IndexPage.as_view()),
+    url(r'^$', views.IndexPage.as_view(), name='index'),
+    url(r'^login/', views.LoginPage.as_view(), name='login'),
+    url(r'^test/', login_required(views.TestPage.as_view()), name='test'),
 ]
--- src/bike_parking/website/views.py
+++ src/bike_parking/website/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 from django.views.generic import View
 from django.shortcuts import render
+from parking.models import ParkingLot
 
 
 class IndexPage(View):
     def get(self, request):
         template = 'website/index.html'
         return render(request, template)
+
+
+class LoginPage(View):
+    def get(self, request):
+        template = 'website/login.html'
+        return render(request, template)
+
+
+class TestPage(View):
+    def get(self, request):
+        parkings = ParkingLot.objects.all()
+        template = 'website/test.html'
+        return render(request, template, {'parkings': parkings})
